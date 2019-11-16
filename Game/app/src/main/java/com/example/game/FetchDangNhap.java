ba7be16ad7ed5681408b8eaf756d6f6c171bf25e
Vo Minh Tan <tanminhvo12340@gmail.com>
2019-11-16 21:43:17 +0700

    Đăng nhập

---
+++ Game/app/src/main/java/com/example/game/FetchDangNhap.java
@@ -0,0 +1,21 @@
+package com.example.game;
+
+import android.os.AsyncTask;
+
+import java.util.HashMap;
+
+public class FetchDangNhap extends AsyncTask <String, Void,String>{
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String uri = strings[0];
+        String method = strings[1];
+        String tendn = strings[2];
+        String matkhau = strings[3];
+
+        HashMap<String, String> params= new HashMap<>();
+        params.put("ten_dang_nhap",tendn);
+        params.put("mat_khau",matkhau);
+        return NetworkUtils.doRequest(uri,method,params,null);
+    }
+}
