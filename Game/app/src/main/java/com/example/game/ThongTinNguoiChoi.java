6697d84cd4585671dec06cc082aacb68feeb07c4
Vo Minh Tan <tanminhvo12340@gmail.com>
2019-11-26 16:38:22 +0700

    Thông tin người chơi

--- Game/app/src/main/java/com/example/game/ThongTinNguoiChoi.java
+++ Game/app/src/main/java/com/example/game/ThongTinNguoiChoi.java
@@ -2,13 +2,69 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ThongTinNguoiChoi extends AppCompatActivity {
 
+    private String sharedPrefFile = "com.example.game";
+    private static String token="";
+    EditText username, email, credit, diemcao;
+    SharedPreferences mPref;
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor prsE = mPref.edit();
+        prsE.putString("TOKEN",token);
+        prsE.apply();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_tin_nguoi_choi);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        credit = findViewById(R.id.credit);
+        diemcao = findViewById(R.id.diemSo);
+        mPref = getSharedPreferences(sharedPrefFile,MODE_PRIVATE);
+        if(mPref.getString("TOKEN",null) !=null) {
+            token = mPref.getString("TOKEN",null);
+            goiAPI();
+        }
+        username.setEnabled(false);
+        credit.setEnabled(false);
+        diemcao.setEnabled(false);
+    }
+    public void goiAPI(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(connectivityManager !=null)
+        {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if(networkInfo != null && networkInfo.isConnected()) {
+            new FectAPIToken() {
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(s);
+                        username.setText(jsonObject.getString("ten_dang_nhap"));
+                        credit.setText(jsonObject.getString("credit"));
+                        diemcao.setText(jsonObject.getString("diem_cao_nhat"));
+                        email.setText(jsonObject.getString("email"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.execute("user-info", "GET", mPref.getString("TOKEN",null));
+        }
     }
 }
